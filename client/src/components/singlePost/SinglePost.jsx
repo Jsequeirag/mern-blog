0986ede15c0cb2885b718609c5b449c140568bcd
Jose Luis Sequeira Góngora <joseluissequeirag2@gmail.com>
2022-07-26 20:07:17 -0600

    production settings

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -22,7 +22,7 @@ export default function SinglePost() {
       });
     };
     getPost();
-  }, [pathname]);
+  }, []);
 
   /* ------------------------------- delete post ------------------------------ */
   const handleDelete = async () => {
